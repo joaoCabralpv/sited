2eeb022256c07b5f8a010d2820e3ac543a4c7646
asus <joaocabralpv@gmail.com>
2024-03-23 14:20:05 -0100

    File io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod interface;
+mod io;
+use crossterm::terminal;
+
+
+
+fn main() ->std::io::Result<()>{   
+    terminal::enable_raw_mode()?;
+    interface::hello();
+    Ok(())
 }
